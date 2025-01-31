ab7be493dcf7e811c111d3c2792e0786fe43a8d7
Côme Mazelier <come.mazelier@etu.minesparis.psl.eu>
2025-01-31 14:18:55 +0100

    camions

--- camion.py
+++ camion.py
@@ -1,12 +1,13 @@
 class Camion:
-    def __init__(self, bouteilles_vides, bouteilles_pleines, position, capacite = 80, deplacement, id, temps_deplacement):
+    def __init__(self, bouteilles_vides, bouteilles_pleines, position, capacite = 80, deplacement, id):
         self._bouteilles_vides = bouteilles_vides
         self._bouteilles_pleines = bouteilles_pleines
         self._position = position
         self._capacite = capacite
         self._deplacement = deplacement
         self._id = id
-        self._temps_deplacement = temps_deplacement
+        self._temps_deplacement = 0
+        self._trajet = []
         
     @property
     def bouteilles_vides(self):
@@ -48,7 +49,11 @@ class Camion:
             else:
                 return False
 
-    def deplacement(self, destination):
+    def deplacement(self, destination, duree):
         self._position = destination
-
-    def  
+        self._trajet.append(destination)
+        self._temps_deplacement = duree
+        
+    def  actualisation(self, t):
+        self._temps_deplacement = self._temps_deplacement - t
+    
