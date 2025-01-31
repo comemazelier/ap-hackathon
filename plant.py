5fecce582fdef96ffabfd172b06ebaff7fd695f2
Adrien Martinez <ad.mrtnz14@gmail.com>
2025-01-31 15:50:58 +0100

    pk

--- plant.py
+++ plant.py
@@ -1,32 +1,30 @@
 class Plant:
-    def __init__(self, coord_x, coord_y, capacite, init, refill, libre):
+    def __init__(self, coord_x, coord_y, capacite, init, refill, libre, id):
         self._coord_x = coord_x
-        self.coord_y = coord_y  # Stocke la position sous forme de tuple (x, y)
+        self._coord_y = coord_y  # Stocke la position sous forme de tuple (x, y)
         self._capacite = capacite         # Capacité maximale de l’usine
-        self._bouteilles_pleines = init                # Nombre actuel de bouteilles pleines en stock
-        self._bouteilles_vides = refill             # Nombre de bouteilles pouvant être remplies par jour
-        self.libre = libre
+        self._bouteilles_pleines = init
+        self._bouteilles_vides = 0               # Nombre actuel de bouteilles pleines en stock
+        self._production = refill             # Nombre de bouteilles pouvant être remplies par jour
+        self._libre = libre
+        self._id = id
         
     
-    def remplir_bouteilles(self, quantite): #remplir un certain nombre de bouteilles jusqu’à la capacité de l’usine.
-        remplissage = min(self.bouteilles_vides, quantite)
-        self.stock += remplissage
-        return remplissage
+    def actualisation(self, dt): #remplir un certain nombre de bouteilles jusqu’à la capacité de l’usine.
+      self._bouteilles_vides = max(0, self._bouteilles_vides - dt*self._production)
+      self._bouteilles_pleines = min(0,self._bouteilles_pleines + dt*self._production)
     
-    def modifier_stock(self, pleines=0, vides=0):
-      if pleines > 0:
-        espace_disponible = self.capacite - self.stock
-        ajout = min(pleines, espace_disponible)  # Ne pas dépasser la capacité
-        self.stock += ajout
-        message_pleines = f"{ajout} bouteilles pleines ajoutées."
-      elif pleines < 0:
-        retrait = min(abs(pleines), self.stock)  # Ne pas retirer plus que ce qui est dispo
-        self.stock -= retrait
-        message_pleines = f"{retrait} bouteilles pleines retirées."
-      else:
-        message_pleines = "Aucune modification de bouteilles pleines."
+    def charge(self, n):
+      if self._bouteilles_vides + n > self._capacite :
+         print('Error')
+      else :
+         self._bouteilles_vides += n
+    
+    def decharge(self, n) :
+      if self._bouteilles_pleines > n :
+         print('Error')
+      else :
+         self._bouteilles_vides += n
 
-      message_vides = f"{vides} bouteilles vides reçues (non stockées ici)."
-      return f"{message_pleines} {message_vides} Stock actuel : {self.stock}/{self.capacite}."
 
 
