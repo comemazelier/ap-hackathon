06464a048850a434a008f4471a1430f956eb73eb
Côme Mazelier <come.mazelier@etu.minesparis.psl.eu>
2025-01-31 10:12:17 +0100

    classe camions

--- bouteilles.py
+++ bouteilles.py
@@ -1,1 +1,8 @@
 import pandas as pd
+
+class Camions:
+    def __init__(self, bouteilles_vides, bouteilles_pleines, position):
+        self.bouteilles_vides = bouteilles_vides
+        self.bouteilles_pleines = bouteilles_pleines
+        self.position = position
+    
