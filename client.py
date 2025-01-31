1f2da9ab5008f30241b54d9975a5a3002a97abec
Tristan Vambert <tristan.vambert@etu.minesparis.psl.eu>
2025-01-31 15:21:05 +0100

    ggg

--- client.py
+++ client.py
@@ -2,28 +2,43 @@
 import pandas as pd
 
 class Client:
-    def __init__(self, coord_x, coord_y, capacity, init, consumption):
-        self.coord_x = int(coord_x)
-        self.coord_y = int(coord_y)
-        self.capacity = int(capacity)
-        self.stock = int(init)
-        self.consumption = float(consumption)
+    def __init__(self, coord_x, coord_y, capacity, init, _consumption):
+        self._coord_x = int(coord_x)
+        self._coord_y = int(coord_y)
+        self._capacity = int(capacity)
+        self._bouteilles_vides = init
+        self._bouteilles = init
+        self._bouteilles_pleines = 0
+        self._libre = True
+        self._consumption = float(_consumption)
     
     def get_data(self):
         return {
-            "coord_x": self.coord_x,
-            "coord_y": self.coord_y,
-            "capacity": self.capacity,
-            "stock": self.stock,
-            "consumption": self.consumption
+            "coord_x": self._coord_x,
+            "coord_y": self._coord_y,
+            "capacity": self._capacity,
+            "stock": self._bouteilles,
+            "consumption": self._consumption
         }
     
     def __repr__(self):
-        return (f"Client(coord_x={self.coord_x}, coord_y={self.coord_y}, "
-                f"capacity={self.capacity}, stock={self.stock}, consumption={self.consumption})")
+        return (f"Client(coord_x={self._coord_x}, coord_y={self._coord_y}, "
+                f"capacity={self._capacity}, stock={self._bouteilles}, consumption={self._consumption})")
     
-    def __evol_stock__(t, self):
-        pass        
+    def actualisation(self,dt):
+        self._bouteilles_pleines = max(0,self._bouteilles_pleines-dt*self._consumption)
+        self._bouteilles_vides = self._bouteilles - self._bouteilles_pleines
+    
+    def charge(self,n,m):
+        if n-m + self._bouteilles > self._capacity :
+            return False
+        elif self._bouteilles_vides - m < 0 :
+            return False
+        else : 
+            self._bouteilles += n-m
+            self._bouteilles_vides -= m
+            self._bouteilles_pleines += n
+               
 
 
 
@@ -35,7 +50,7 @@ class ClientsList:
         clients = []
         df = pd.read_csv(filename)  
         for _, row in df.iterrows(): 
-            clients.append(Client(row["coord_x"], row["coord_y"], row["capacity"], row["init"], row["consumption"]))
+            clients.append(Client(row["coord_x"], row["coord_y"], row["capacity"], row["init"], row["_consumption"]))
         return clients
     
     def get_all_clients_data(self):
@@ -44,6 +59,5 @@ class ClientsList:
     def __repr__(self):
         return f"ClientsList({self.clients})"
 
-# Exemple d'utilisation
 clients_list = ClientsList("clients.csv")
 print(clients_list.get_all_clients_data())
