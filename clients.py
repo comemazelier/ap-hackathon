c0d699853138c090676b390b345cd39a91856c39
Tristan Vambert <tristan.vambert@etu.minesparis.psl.eu>
2025-01-31 12:13:20 +0100

    clients trstan

---
+++ clients.py
@@ -0,0 +1,51 @@
+
+import pandas as pd
+
+class Client:
+    def __init__(self, coord_x, coord_y, capacity, init, consumption):
+        self.coord_x = int(coord_x)
+        self.coord_y = int(coord_y)
+        self.capacity = int(capacity)
+        self.stock = int(init)
+        self.consumption = float(consumption)
+    
+    def get_data(self):
+        return {
+            "coord_x": self.coord_x,
+            "coord_y": self.coord_y,
+            "capacity": self.capacity,
+            "stock": self.stock,
+            "consumption": self.consumption
+        }
+    
+    def __repr__(self):
+        return (f"Client(coord_x={self.coord_x}, coord_y={self.coord_y}, "
+                f"capacity={self.capacity}, stock={self.stock}, consumption={self.consumption})")
+    
+    def __evol_stock__(t, self):
+        
+
+        
+
+
+
+class ClientsList:
+    def __init__(self, filename):
+        self.clients = self.load_clients(filename)
+    
+    def load_clients(self, filename):
+        clients = []
+        df = pd.read_csv(filename)  
+        for _, row in df.iterrows(): 
+            clients.append(Client(row["coord_x"], row["coord_y"], row["capacity"], row["init"], row["consumption"]))
+        return clients
+    
+    def get_all_clients_data(self):
+        return [client.get_data() for client in self.clients]
+    
+    def __repr__(self):
+        return f"ClientsList({self.clients})"
+
+# Exemple d'utilisation
+clients_list = ClientsList("clients.csv")
+print(clients_list.get_all_clients_data())
