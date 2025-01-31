80632077747adaeb7542df15980a7926acbd2c9e
Adrien Martinez <ad.mrtnz14@gmail.com>
2025-01-31 13:36:45 +0100

    réorganisation

--- flotte.py
+++ flotte.py
@@ -3,7 +3,7 @@ from clients import Client
 from plant import Plant
 
 class Flotte :
-    def __init__(self, clients : 'list[Client]', usines ):
+    def __init__(self, clients : 'list[Client]', usines, clients_filename, plants_filename ):
         self.camions = []
         self.clients = clients
         self.plants = usines
@@ -18,9 +18,14 @@ class Flotte :
         for client in self.clients :
             client.actualisation(t)
         for usine in self.usines :
-            usines.actualisation(t)
+            usine.actualisation(t)
 
-
+    def load_clients(self, filename):
+        clients = []
+        df = pd.read_csv(filename)  
+        for _, row in df.iterrows(): 
+            clients.append(Client(row["coord_x"], row["coord_y"], row["capacity"], row["init"], row["consumption"]))
+        return clients
         
 
     def calcul_destinations() :
