b8b645a94d5224b191391eb91702cd0d226bf64c
Honoré Boiarsky <honore.boiarsky@etu.minesparis.psl.eu>
2025-01-31 10:07:03 +0100

    class plant

--- bouteilles.py
+++ bouteilles.py
@@ -1,1 +1,20 @@
 import pandas as pd
+
+
+
+class Plant:
+    def __init__(self, coord_x, coord_y, capacity, init, refill):
+        self.coord = (coord_x, coord_y)
+        self.capacity = capacity
+        self.stock = init
+        self.refill = refill
+    
+    def remplir_bouteilles(self, quantite):
+        remplissage = min(self.refill, quantite)
+        self.stock += remplissage
+        return remplissage
+    
+
+
+
+
