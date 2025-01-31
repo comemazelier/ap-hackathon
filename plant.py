86666c1d98b939c740f5d5fcb24dbab5008ad8f1
Honoré Boiarsky <honore.boiarsky@etu.minesparis.psl.eu>
2025-01-31 12:13:06 +0100

    merge

--- plant.py
+++ plant.py
@@ -5,7 +5,28 @@ class Plant:
         self.stock = init                # Nombre actuel de bouteilles pleines en stock
         self.refill = refill             # Nombre de bouteilles pouvant être remplies par jour
     
-    def remplir_bouteilles(self, quantite): #remplir un certain nombre de bouteilles jusqu’à la capacité de l’usine.
-        remplissage = min(self.refill, quantite)
+    def remplir_bouteilles(self): #remplir un certain nombre de bouteilles jusqu’à la capacité de l’usine.
+        remplissage = min(self.refill, self.capacity - self.stock)
         self.stock += remplissage
-        return remplissage
+    
+    def modifier_stock(self, pleines=0, vides=0):
+      if pleines > 0:
+        espace_disponible = self.capacity - self.stock
+        ajout = min(pleines, espace_disponible)  # Ne pas dépasser la capacité
+        self.stock += ajout
+        message_pleines = f"{ajout} bouteilles pleines ajoutées."
+      elif pleines < 0:
+        retrait = min(abs(pleines), self.stock)  # Ne pas retirer plus que ce qui est dispo
+        self.stock -= retrait
+        message_pleines = f"{retrait} bouteilles pleines retirées."
+      else:
+        message_pleines = "Aucune modification de bouteilles pleines."
+
+    # Message pour les bouteilles vides (non stockées dans l’usine)
+      message_vides = f"{vides} bouteilles vides reçues (non stockées ici)."
+
+      return f"{message_pleines} {message_vides} Stock actuel : {self.stock}/{self.capacity}."
+
+
+
+
--- trajets.py
+++ trajets.py
@@ -4,8 +4,20 @@ import numpy as np
 clients = pd.read_csv('clients.csv')
 plants = pd.read_csv('plants.csv')
 
-print(clients)
-print(plants)
+# Paramètres
+NUM_CAMIONS = 100
+CAP_CAMION = 80
+VITESSE = 50  # km/h
+COUT_KM = 0.10  # AC/km
+PRIX_VENTE = 100  # AC/bouteille
+COUT_REMPLISSAGE = 40  # AC/bouteille
+DUREE_SIMULATION = 30  # jours
+
+def distance (p1, p2):
+    return np.linalg.norm(np.array(p1) - np.array(p2))
+
+
+
 
 
     
